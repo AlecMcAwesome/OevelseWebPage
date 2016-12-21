85e8dff8a5979f83f372032c5ea70650ccfc82a4
AlecMcAwesome <fredi-petersen93@hotmail.com>
2016-12-21 18:42:01 +0100

    login html lavet og retter vertikalt input

--- src/JavaScript/FunctionsForMain.js
+++ src/JavaScript/FunctionsForMain.js
@@ -1,1 +1,12 @@
+function saveUserInfo(){
+    var userName = document.getElementById("usr").value;
+    var passW = document.getElementById("pwd").value;
+    var emailA = document.getElementById("email").value;
 
+    console.log(userName + passW + emailA);
+
+    if(userName == "lol" && passW == "lol"){
+        alert("Sign up successfull")
+    }
+
+}
